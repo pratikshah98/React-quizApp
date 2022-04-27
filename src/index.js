2015776b8db5f469607e8d022a18ecb89db4cb21
pratik <pratik@selcom.net>
2022-04-27 11:52:51 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{ Component } from "react";
+import ReactDOM  from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+class QuizBee extends Component{
+    state ={
+        questionBank : [],
+        score:0,
+        responses :0
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    }
+    getQuestion =() =>{
+        quizService().then(question =>{
+            this.setState({
+                questionBank :question
+            });
+        });
+    };
+    computeAnswer =(answer,correctAnswer) =>{
+        if(answer === correctAnswer){
+            this.setState({
+                score: this.state.score + 1 
+            })
+        }
+        this.setState({
+            responses:this.state.responses < 5 ? this.state.responses + 1 : 5 
+        })
+    }
+    componentDidMount(){
+        this.getQuestion();
+    }
+     divStyle = {
+        color: 'blue',
+        //backgroundImage: 'url(' + imgUrl + ')',
+      };
+    render(){
+        return(
+            <div className="container" style={{width:'540px'}}>
+                <div className="title" style={{marginTop:'50px'}}>Quizbee</div>
+                {this.state.questionBank.length > 0
+                && this.state.responses < 5
+                && this.state.questionBank.map(
+                    ({question,answers,correct,questionId})=>(
+                    <QuestionBox 
+                    question={question} 
+                    options={answers}
+                    key={questionId}
+                    selected={answer =>{
+                        this.computeAnswer(answer,correct)
+                    }}
+                    />
+                    )
+            )}
+            {this.state.responses === 5 ? (<h2>{this.state.score} </h2>) : null }
+            </div>
+        )
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<QuizBee />,document.getElementById("root"));
